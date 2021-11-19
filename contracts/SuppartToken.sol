b39186a75c5832c3d392139891a4ab1954f4f17a
msierracrypto <msierra@gmail.com>
2021-11-18 20:18:51 -0500

    SuppartToken deleete function

--- contracts/SuppartToken.sol
+++ contracts/SuppartToken.sol
@@ -3,29 +3,35 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract GoFundMeToken is ERC20, ERC20Detailed {
-    
-    struct Project {
-        string name;
-        uint total;
-    }
-    
-    Project[] public ProjectTotals;
-    
-    struct AddressFunding {
-        string name;
-        uint total;
-    }
-    
-    mapping(address => Project[]) projects;
-    
-    address private owner;
+contract SuppartToken is ERC20, ERC20Detailed {
     
     event TokenBuyEvent (
         address from,
         address to,
         uint256 amount
     );
+    
+    event TokeSellEvent (
+        address from,
+        address to,
+        uint256 amount
+    );
+    
+    struct Project {
+        string name;
+        uint total;
+    }
+    
+    struct AddressFunding {
+        string name;
+        uint total;
+    }
+    
+    mapping(address => AddressFunding[]) AddressProjects;
+    Project[] ProjectTotals;
+    
+    address private owner;
+    mapping (address => uint256) pendingWithdrawals;
     
     constructor(
         string memory name,
@@ -40,8 +46,10 @@ contract GoFundMeToken is ERC20, ERC20Detailed {
     }
     
     function buyToken(string calldata _projectName, uint256 _amount) external payable {
+        
         require(_amount == ((msg.value / 1 ether)), "Incorrect amount of Eth.");
-        //require(project_name, "You must provide a project_name");
+        //require(_projectName, "You must provide a project_name");
+        
         bool foundproject;
         uint project_index;
         
@@ -51,12 +59,51 @@ contract GoFundMeToken is ERC20, ERC20Detailed {
         
         if(foundproject) {
             ProjectTotals[project_index].total += _amount;
+        } else {
+            createSupportProject(_projectName);
         }
         
         emit TokenBuyEvent(owner, msg.sender, _amount);
     }
     
-    function createSupportProject(string memory _projectName) public returns(bool result){ 
+    function sellToken(string memory _projectName, uint256 _amount) public {
+        
+        bool foundproject;
+        uint project_index;
+        
+        (foundproject, project_index) = getProjectIndex(_projectName);
+        
+        require(foundproject == true, 'You must provide a valid _projectName');
+        
+        AddressFunding[] memory addressfunding = AddressProjects[msg.sender];
+        
+        for(uint i = 0; i< addressfunding.length; i++) {
+            
+            if(keccak256(bytes(addressfunding[i].name)) == keccak256(bytes(_projectName))){
+                
+                //found project for this address
+                addressfunding[i].total -= _amount;
+                
+            }
+        }
+        
+        pendingWithdrawals[msg.sender] = _amount;
+        
+        transfer(owner, _amount);
+        
+        withdrawEth();
+        
+        emit TokeSellEvent(msg.sender, owner, _amount);
+    }
+    
+    function withdrawEth() public {
+        uint256 amount = pendingWithdrawals[msg.sender];
+        // Pending refund zerod before to prevent re-entrancy attacks
+        pendingWithdrawals[msg.sender] = 0;
+        msg.sender.transfer(amount * 1 ether);
+    }
+    
+    function createSupportProject(string memory _projectName) public returns(bool result, uint id){ 
         bool foundproject;
         
         (foundproject, ) = getProjectIndex(_projectName);
@@ -67,7 +114,9 @@ contract GoFundMeToken is ERC20, ERC20Detailed {
             
         ProjectTotals.push(_newProject);
         
-        return true;
+        id = ProjectTotals.length - 1;
+        
+        return (true, id);
     }
     
     function getProjectIndex(string memory project_name) internal view returns(bool foundproject, uint project_index){
